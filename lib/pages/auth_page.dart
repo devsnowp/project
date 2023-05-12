83d05c066460d5a3002922994c4ade23e99fa45c
devsnowp <devsnowp@gmail.com>
2023-05-12 10:26:23 -0300

    firebase auth introduction

---
+++ lib/pages/auth_page.dart
@@ -0,0 +1,24 @@
+import "package:firebase_auth/firebase_auth.dart";
+import "package:flutter/material.dart";
+import "package:project/pages/home_page.dart";
+import 'package:firebase_core/firebase_core.dart';
+import "package:project/pages/login_page.dart";
+
+class AuthPage extends StatelessWidget {
+  const AuthPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: StreamBuilder<User?>(
+          stream: FirebaseAuth.instance.authStateChanges(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return HomePage();
+            } else {
+              return LoginPage();
+            }
+          }),
+    );
+  }
+}
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -15,113 +15,116 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.pink[600],
-      body: SafeArea(
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const SizedBox(height: 30),
-              Icon(
-                Icons.open_with,
-                size: 100,
-                color: Colors.tealAccent[400],
-              ),
-              const SizedBox(height: 30),
-              const Text(
-                'Bem-vindo!',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 18,
+      body: SingleChildScrollView(
+        reverse: true,
+        child: SafeArea(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const SizedBox(height: 30),
+                Icon(
+                  Icons.open_with,
+                  size: 100,
+                  color: Colors.tealAccent[400],
                 ),
-              ),
-              const SizedBox(height: 30),
-              MyTextField(
-                controller: usernameController,
-                hintText: 'Username',
-                obscureText: false,
-              ),
-              const SizedBox(height: 30),
-              MyTextField(
-                controller: passwordController,
-                hintText: 'Password',
-                obscureText: true,
-              ),
-              const SizedBox(height: 10),
-              const Padding(
-                padding: EdgeInsets.symmetric(horizontal: 25),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    Text(
-                      'Esqueceu a senha?',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 14,
-                      ),
-                    )
-                  ],
+                const SizedBox(height: 30),
+                const Text(
+                  'Bem-vindo!',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                  ),
                 ),
-              ),
-              const SizedBox(height: 30),
-              MyButton(
-                onTap: signIn,
-              ),
-              const SizedBox(height: 30),
-              const Padding(
-                padding: EdgeInsets.symmetric(horizontal: 25),
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: Divider(
-                        thickness: 1,
-                        color: Colors.tealAccent,
-                      ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 10),
-                      child: Text(
-                        "Ou continue com",
+                const SizedBox(height: 30),
+                MyTextField(
+                  controller: usernameController,
+                  hintText: 'Username',
+                  obscureText: false,
+                ),
+                const SizedBox(height: 30),
+                MyTextField(
+                  controller: passwordController,
+                  hintText: 'Password',
+                  obscureText: true,
+                ),
+                const SizedBox(height: 10),
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 25),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Text(
+                        'Esqueceu a senha?',
                         style: TextStyle(
                           color: Colors.white,
-                          fontWeight: FontWeight.w500,
+                          fontSize: 14,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 30),
+                MyButton(
+                  onTap: signIn,
+                ),
+                const SizedBox(height: 30),
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 25),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: Divider(
+                          thickness: 1,
+                          color: Colors.tealAccent,
                         ),
                       ),
-                    ),
-                    Expanded(
-                      child: Divider(
-                        thickness: 1,
-                        color: Colors.tealAccent,
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 10),
+                        child: Text(
+                          "Ou continue com",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
                       ),
-                    )
+                      Expanded(
+                        child: Divider(
+                          thickness: 1,
+                          color: Colors.tealAccent,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 30),
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SquareTile(imagePath: 'lib/images/google.png'),
+                    const SizedBox(width: 10),
+                    SquareTile(imagePath: 'lib/images/facebook.png'),
                   ],
                 ),
-              ),
-              const SizedBox(height: 30),
-              const Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  SquareTile(imagePath: 'lib/images/google.png'),
-                  const SizedBox(width: 10),
-                  SquareTile(imagePath: 'lib/images/facebook.png'),
-                ],
-              ),
-              const SizedBox(height: 50),
-              const Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    "Não é cadastrado?",
-                    style: TextStyle(color: Colors.white),
-                  ),
-                  SizedBox(width: 5),
-                  Text("Se cadastre",
-                      style: TextStyle(
-                        color: Colors.blue,
-                        fontWeight: FontWeight.bold,
-                      )),
-                ],
-              )
-            ],
+                const SizedBox(height: 50),
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Não é cadastrado?",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    SizedBox(width: 5),
+                    Text("Se cadastre",
+                        style: TextStyle(
+                          color: Colors.blue,
+                          fontWeight: FontWeight.bold,
+                        )),
+                  ],
+                )
+              ],
+            ),
           ),
         ),
       ),
